bf8d846382e5004af88c90add84abe82091d9933
ambi hidalgo <ambio.pk@gmail.com>
2023-07-06 15:21:22 -0700

    Initial Commit - Begin planning

---
+++ capstone/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View></View>
+      //HomeScreen will act as the home-base that will display user profile data cards, hold the navbar.
+    // <HomeScreen>
+        //<ProfileCard/>
+        //<NavBar>
+        //<HomeScreen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
