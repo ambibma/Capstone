ba241dbb2e0c84aa059070128a1013caa1487d14
ambi hidalgo <ambio.pk@gmail.com>
2023-07-13 19:36:29 -0700

    Add bottomTab Navigation

--- capstone/App.js
+++ capstone/App.js
@@ -1,9 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons'
+import HomeScreen from './screens/HomeScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+
+
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View></View>
+    
+    <NavigationContainer>
+      <BottomTab.Navigator>
+        <BottomTab.Screen
+          name="HomeScreen"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: (({color, size})=> <Ionicons name="home" color={color} size={size}></Ionicons>)
+          }}
+        />
+        <BottomTab.Screen
+        name="ProfileScreen" 
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: (({color, size})=> <Ionicons name="person" color={color} size={size}></Ionicons>)
+        }}
+        />
+
+
+
+
+
+      </BottomTab.Navigator>
+
+
+    </NavigationContainer>
       //HomeScreen will act as the home-base that will display user profile data cards, hold the navbar.
     // <HomeScreen>
         //<ProfileCard/>
